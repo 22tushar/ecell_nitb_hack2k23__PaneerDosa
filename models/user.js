04d0bc2171aa1e2bab461a3f55cac759fc8b052b
22tushar <gehlottushar18@gmail.com>
2023-02-11 03:48:50 +0530

    add backend

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    role:{type:String},
+    name:{type:String},
+    desc:{type:String},
+    email:{type:String},
+    password:{type:String},
+    isAdmin: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+exports.User = User;
